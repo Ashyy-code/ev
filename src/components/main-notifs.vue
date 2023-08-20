0b35f1ab4079176e491156934a0a2932fc470a3d
Ash Lawson <108193997+Ashyy-code@users.noreply.github.com>
2023-08-20 11:10:10 +0100

    remove popup feedback during development build

--- src/components/main-notifs.vue
+++ src/components/main-notifs.vue
@@ -11,8 +11,8 @@ export default {
 
     data(){
         return{
-            popupVis:true,
-            showFeedbackPopup:true,
+            popupVis:false,
+            showFeedbackPopup:false,
         }
     }
 
